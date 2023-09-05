298ef6d9ae86ea62be2f43ed282eeb6a3c305c6a
nezhataghy <nezhataghy@gmail.com>
2023-09-05 13:28:30 +0000

    python-IF-ELSE-LOOP-FUNCTIONS

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for num in range(0, 100):
     if num != 99:
-        print("{}, ".format(num:02), end='')
+        print("{:02}, ".format(num), end='')
 
     else:
         print("{}".format(num))
