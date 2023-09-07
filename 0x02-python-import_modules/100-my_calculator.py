3b671074f6ba00bb3f1eb229d026c4e8992b6f53
nezhataghy <nezhataghy@gmail.com>
2023-09-07 18:30:40 +0000

    IMPORT MODULES

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+    
+
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    print("{} {} {} = {}".format(a, argv[2], b, ops[argv[2]](a, b)))
