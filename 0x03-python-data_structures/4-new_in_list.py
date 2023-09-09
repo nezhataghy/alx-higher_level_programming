5f3cd2cd70afb07ea7bf7f5e9dd37201cd80f056
nezhataghy <nezhataghy@gmail.com>
2023-09-09 16:16:12 +0000

    DATA STRUCTURES

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
+def new_in_list(my_list, idx, element):
     copy = my_list.copy()
     if idx >= 0 and idx <= (len(my_list) - 1):
         copy[idx] = element
