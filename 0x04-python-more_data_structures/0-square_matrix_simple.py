50f7b11476c1b2458379177d3bc4c79fac4c8bdf
nezhataghy <nezhataghy@gmail.com>
2023-09-13 16:54:51 +0000

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,9 +2,9 @@
 def square_matrix_simple(matrix=[]):
 
     new_matrix = []
-    for l in range(len(matrix)):
+    for line in range(len(matrix)):
         temp = []
-        for c in range(len(matrix[l])):
-            temp.append(matrix[l][c] ** 2)
+        for c in range(len(matrix[line])):
+            temp.append(matrix[line][c] ** 2)
         new_matrix.append(temp)
     return (new_matrix)
