b6b7f567cb46836fc3d33b3d4575962cb12ac4a1
nezhataghy <nezhataghy@gmail.com>
2023-09-13 19:48:08 +0000

    a_dictionary as a list

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        return (max(a_dictionary))
+        max = list(a_dictionary.keys())[0]
+        for i in a_dictionary:
+            if a_dictionary[i] > a_dictionary[max]:
+                max = i
+        return (max)
+    return (None)
