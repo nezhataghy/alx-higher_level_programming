8fedd629d8ecd56ba02e5f271a97a9640e5dc14a
nezhataghy <nezhataghy@gmail.com>
2023-09-14 01:30:21 +0000

    final test

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -37,30 +37,30 @@ void print_python_bytes(PyObject *p)
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
+}
+/**
+ * print_python_list - Prints basic info about Python lists.
+ * @p: A PyObject list object.
+ */
+void print_python_list(PyObject *p)
+{
+	int size, alloc, i;
+	const char *type;
+	PyListObject *list = (PyListObject *)p;
+	PyVarObject *var = (PyVarObject *)p;
 
-	/**
-	 * print_python_list - Prints basic info about Python lists.
-	 * @p: A PyObject list object.
-	 */
-	void print_python_list(PyObject * p)
+	size = var->ob_size;
+	alloc = list->allocated;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
+
+	for (i = 0; i < size; i++)
 	{
-		int size, alloc, i;
-		const char *type;
-		PyListObject *list = (PyListObject *)p;
-		PyVarObject *var = (PyVarObject *)p;
-
-		size = var->ob_size;
-		alloc = list->allocated;
-
-		printf("[*] Python list info\n");
-		printf("[*] Size of the Python List = %d\n", size);
-		printf("[*] Allocated = %d\n", alloc);
-
-		for (i = 0; i < size; i++)
-		{
-			type = list->ob_item[i]->ob_type->tp_name;
-			printf("Element %d: %s\n", i, type);
-			if (strcmp(type, "bytes") == 0)
-				print_python_bytes(list->ob_item[i]);
-		}
+		type = list->ob_item[i]->ob_type->tp_name;
+		printf("Element %d: %s\n", i, type);
+		if (strcmp(type, "bytes") == 0)
+			print_python_bytes(list->ob_item[i]);
 	}
+}
