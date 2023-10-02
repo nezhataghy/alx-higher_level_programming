c490e4f4a3589e472feb5d4531b6680d5c17f08d
nezhataghy <nezhataghy@gmail.com>
2023-10-02 21:28:00 +0000

    documentation

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+'''Defines a Rectangle class'''
 
 
 class Rectangle:
     def __init__(self, width=0, height=0):
-        """Represent a rectangle."""
+        '''Represent a rectangle.'''
         self.height = height
         self.width = width
 
     @property
     def width(self):
+        '''width'''
         return self.__width
 
     @width.setter
     def width(self, value):
+        '''width'''
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -22,10 +24,12 @@ class Rectangle:
 
     @property
     def height(self):
+        '''height'''
         return self.__height
 
     @height.setter
     def height(self, value):
+        '''height'''
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
