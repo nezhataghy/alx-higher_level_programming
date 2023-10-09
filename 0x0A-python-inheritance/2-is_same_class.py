33765b2abeb09735050eed84840dc024890ed766
nezhataghy <nezhataghy@gmail.com>
2023-10-09 22:59:17 +0000

    update

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,4 +5,6 @@
 def is_kind_of_class(obj, a_class):
     """ Function that returns True/False if obj is an instance of a_class
     """
-    return isinstance(obj, a_class)
+    if not isinstance(a_class, type):
+        raise TypeError("a_class must be of type 'type'")
+    return (type(obj) is a_class)
