5a06207c1ac1739d705630dbacc35fa859fe2c0b
nezhataghy <nezhataghy@gmail.com>
2023-10-09 22:57:32 +0000

    Exact same object

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines a class-checking function"""
+
+
 def is_kind_of_class(obj, a_class):
     """ Function that returns True/False if obj is an instance of a_class
     """
