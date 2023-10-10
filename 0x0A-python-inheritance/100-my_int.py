eccefd1d65d84814f21d3669955841234f276c3f
nezhataghy <nezhataghy@gmail.com>
2023-10-10 00:03:22 +0000

    advanced

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+'''
+    Class that takes an integers
+'''
+
+
+class MyInt(int):
+    """ Class that inherits from class int"""
+
+    def __eq__(self, other):
+        """ Method that returns != check """
+        return int.__ne__(self, other)
+
+    def __ne__(self, other):
+        """ Method that returns == check """
+        return int.__eq__(self, other)
