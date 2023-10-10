cfaee7eadc3f26ab6409b88274c2f656be7618f2
nezhataghy <nezhataghy@gmail.com>
2023-10-10 23:33:40 +0000

    input output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""loads adds saves json to file"""
+"""loads adds saves json to the file"""
 import json
 import sys
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
