29a09b0ecde56c6892c44f9d339d8dccadcb25ab
nezhataghy <nezhataghy@gmail.com>
2023-12-11 17:26:18 +0000

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const args = process.argv.length;
+if (args > 3) {
+  console.log('Arguments found');
+} else if (args === 3) {
+  console.log('Argument found');
+} else {
+  console.log('No Argument');
+}
