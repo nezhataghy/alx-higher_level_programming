62b9780168011c2063086c8926b37a343ef0925b
nezhataghy <nezhataghy@gmail.com>
2023-12-11 18:47:31 +0000

    I loooove C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const numb = process.argv[2];
+let n = 0;
+
+if (isNaN(parseInt(process.argv[2]))) {
+  console.log('Missing number of occurrences');
+} else {
+  while (n < numb) {
+    console.log('C is fun');
+    n++;
+  }
+}
