a82f947e6ee7a750be54a636641a5a11e6e4cf65
nezhataghy <nezhataghy@gmail.com>
2023-12-12 21:07:57 +0000

    Rectangle #4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,32 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let ligne = '';
+      for (let j = 0; j < this.width; j++) {
+        ligne += 'X';
+      }
+      console.log(ligne);
+    }
+  }
+
+  rotate () {
+    const variable = this.height;
+    this.height = this.width;
+    this.width = variable;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+
+module.exports = Rectangle;
