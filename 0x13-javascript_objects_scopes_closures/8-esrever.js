397d6abe98d9c174f8ef1c578cd9f73d9f6ca0c0
nezhataghy <nezhataghy@gmail.com>
2023-12-12 21:46:22 +0000

    Esrever

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  const listReverse = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    listReverse.push(list[i]);
+  }
+  return listReverse;
+};
