6437731f81b198f5aea4dd9399ef4c0ef78206d5
nezhataghy <nezhataghy@gmail.com>
2023-12-12 22:36:41 +0000

    Sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const dict = require('./101-data.js').dict;
+let my_Dict = {};
+for (let key in dict) {
+  if (my_Dict[dict[key]] === undefined) {
+    my_Dict[dict[key]] = [key];
+  } else {
+    my_Dict[dict[key]].push(key);
+  }
+}
+console.log(my_Dict);
