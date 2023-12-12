b8d9b6713d846e1cead031c2754b66c16fe63424
nezhataghy <nezhataghy@gmail.com>
2023-12-12 22:41:27 +0000

    Concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+const fs = require('fs');
+
+const file1 = fs.readFileSync(process.argv[2], 'utf-8').toString();
+const file2 = fs.readFileSync(process.argv[3], 'utf-8').toString();
+fs.writeFileSync(process.argv[4], file1 + file2);
