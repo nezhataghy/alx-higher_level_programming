ee7b78dd14fc5b9df9cc7f29b010471389e5c8c0
nezhataghy <nezhataghy@gmail.com>
2023-12-12 22:37:32 +0000

    semistandard :)

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-let my_Dict = {};
-for (let key in dict) {
+const my_Dict = {};
+for (const key in dict) {
   if (my_Dict[dict[key]] === undefined) {
     my_Dict[dict[key]] = [key];
   } else {
