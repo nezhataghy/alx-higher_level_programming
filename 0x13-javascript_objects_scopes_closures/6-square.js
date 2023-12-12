5afe20fa874722e31df384e78fdafc2398e9e948
nezhataghy <nezhataghy@gmail.com>
2023-12-12 21:14:12 +0000

    Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const SquarePants = require('./5-square');
+
+class Square extends SquarePants {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.height));
+    }
+  }
+}
+module.exports = Square;
