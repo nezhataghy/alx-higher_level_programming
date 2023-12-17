4d18046a67c944449a8f4147d165485c820dd85d
nezhataghy <nezhataghy@gmail.com>
2023-12-17 19:31:59 +0000

    SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+script that takes in an argument and displays all values in the states table
+ of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    con = MySQLdb.connect(host="localhost",
+                          port=3306, user=argv[1],
+                          passwd=argv[2], db=argv[3])
+
+    cursor = con.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id;".format(argv[4]))
+    records = cursor.fetchall()
+    for row in records:
+        if row[1] == argv[4]:
+            print(row)
+    cursor.close()
+    con.close()
