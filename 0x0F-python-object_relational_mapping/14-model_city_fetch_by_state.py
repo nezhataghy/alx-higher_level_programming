4af27e7916a71b479802f823c6aec9fa876d4ce4
nezhataghy <nezhataghy@gmail.com>
2023-12-18 01:13:38 +0000

    Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -7,6 +7,7 @@ from model_state import Base, State
 from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
 
 if __name__ == "__main__":
     con = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
