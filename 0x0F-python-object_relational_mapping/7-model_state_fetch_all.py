bc73b7f7b22a40557cba600d612ea3669e51067c
nezhataghy <nezhataghy@gmail.com>
2023-12-18 00:10:34 +0000

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
