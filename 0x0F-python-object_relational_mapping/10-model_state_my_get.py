62b73dda5f23bb9700b9351574a87bf580fbf0ec
nezhataghy <nezhataghy@gmail.com>
2023-12-18 00:50:44 +0000

    Contains `a`

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-script that prints the first State object that contain the letter a 
-from the database hbtn_0e_6_usa
+script that prints the first State object from the database hbtn_0e_6_usa
 """
 from sys import argv
 from model_state import Base, State
