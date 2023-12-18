922fa2e6e7c5d0fafb73e99b0144f27926b7045c
nezhataghy <nezhataghy@gmail.com>
2023-12-18 02:01:38 +0000

    pep8

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -10,11 +10,11 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
-          argv[1], argv[2], argv[3]))
+        argv[1], argv[2], argv[3]))
     Base.metadata.create_all(eng)
     Session = sessionmaker(bind=eng)
     session = Session()
     demand = session.query(City).order_by(City.id).all()
     for row in demand:
-            print("{}: {} -> {}".format(row.id, row.name, row.state.name))
+        print("{}: {} -> {}".format(row.id, row.name, row.state.name))
     session.close()
