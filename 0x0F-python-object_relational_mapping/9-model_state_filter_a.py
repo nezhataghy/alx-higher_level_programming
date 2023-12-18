8f1f95aa98c7101c9bc1e1703225d59f452cd3c6
nezhataghy <nezhataghy@gmail.com>
2023-12-18 00:40:33 +0000

    Contains `a`

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+script that prints the first State object from the database hbtn_0e_6_usa
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    con = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    my_eng = create_engine(con.format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=my_eng)
+    session = Session()
+    demand = session.query(State).filter(
+        State.name.ilike("%a%")).order_by(State.id).all()
+    for item in demand:
+        print("{}: {}".format(item.id, item.name))
