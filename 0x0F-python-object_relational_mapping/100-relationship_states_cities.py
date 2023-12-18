0a539f97406fb0eed7bd1de60efe9ffc3b9b0ec7
nezhataghy <nezhataghy@gmail.com>
2023-12-18 01:29:56 +0000

    City relationship

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     con = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
     my_eng = create_engine(con.format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
+    Base.metadata.create_all(my_eng)
     Session = sessionmaker(bind=my_eng)
     session = Session()
     n_city = City(name="San Francisco")
