094e76e974026f4e4c4781d5bb34ef2aaff44b47
nezhataghy <nezhataghy@gmail.com>
2024-01-07 23:37:27 +0000

    POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+
+if __name__ == "__main__":
+    import sys
+    from urllib.request import Request, urlopen
+    from urllib.parse import urlencode
+
+    url = sys.argv[1]
+    valeurs = {'email': sys.argv[2]}
+    donnee = urlencode(valeurs)
+    donnee = donnee.encode('ascii')
+    demande = Request(url, donnee)
+
+    with urlopen(demande) as reponse:
+        print(reponse.read().decode())
