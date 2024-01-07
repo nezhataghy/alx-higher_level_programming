59bb4c6c3e6a6fdd8c479adbb6df98ccaa8be13d
nezhataghy <nezhataghy@gmail.com>
2024-01-07 23:29:56 +0000

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+
+if __name__ == "__main__":
+    import sys
+    from urllib.request import Request, urlopen
+    url = sys.argv[1]
+    demande = Request(url)
+    with urlopen(demande) as response:
+        reponse = response.getheaders()
+
+    for header in reponse:
+        if header[0] == "X-Request-Id":
+            print(header[1])
+
+    #with urllib.request.urlopen(url) as response:
+    #   print(response.headers.get('X-Request-Id'))
