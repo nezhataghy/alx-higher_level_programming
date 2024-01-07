4420b2725ee2a75471cc8846e4d574bd16c55071
nezhataghy <nezhataghy@gmail.com>
2024-01-07 23:50:04 +0000

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+
+if __name__ == "__main__":
+    import sys
+    from urllib.request import Request, urlopen
+    from urllib.error import HTTPError
+
+    url = sys.argv[1]
+    demande = Request(url)
+    try:
+        with urlopen(demande) as reponse:
+            print(reponse.read().decode())
+    except HTTPError as error:
+        print(f'Error code: {error.code}')
