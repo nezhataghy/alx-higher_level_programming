9b388905c116a3e523baea9dfb43fdadadcf1253
nezhataghy <nezhataghy@gmail.com>
2024-01-08 00:11:42 +0000

    POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status"""
+
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    donnee = {"email": email}
+    dmnd = requests.post(url, donnee)
+    print(dmnd.text)
