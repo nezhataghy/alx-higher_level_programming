41cc5faa282054a1386d9e411788c305f6e84b2e
nezhataghy <nezhataghy@gmail.com>
2024-01-08 00:47:12 +0000

    My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status"""
+
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = "https://api.github.com/user"
+    reponse = requests.get(url, auth=(username, password))
+    try:
+        print(reponse.json()["id"])
+    except KeyError:
+        print("None")
