5458ac5e6a7606d69f1f643be90862bc3f864239
nezhataghy <nezhataghy@gmail.com>
2024-01-08 00:15:07 +0000

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status"""
+
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    reponse = requests.get(sys.argv[1])
+    if (reponse.status_code >= 400): # error
+        print(f"Error code: {reponse.status_code}")
+    else:
+        print(reponse.text)
