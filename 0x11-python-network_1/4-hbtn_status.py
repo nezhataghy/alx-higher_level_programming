d5a634e3925f35c631cbfeb27dc24b4cda8cd21e
nezhataghy <nezhataghy@gmail.com>
2024-01-08 00:02:06 +0000

    What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""Python script thatv fetches https://alx-intranet.hbtn.io/status"""
-
+"""Fetches https://alx-intranet.hbtn.io/status"""
+import requests
 
 if __name__ == "__main__":
-    import requests
-
-    reponse = requests.get("https://alx-intranet.hbtn.io/status")
-    print("Body reponse:")
-    print("\t- type: {}".format(type(reponse.text)))
-    print("\t- content: {}".format(reponse.text))
+    req = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print(f"\t- type: {type(req.text)}")
+    print(f"\t- content: {req.text}")
