9731c092302262805ee7e49cd0375c723cd63c62
nezhataghy <nezhataghy@gmail.com>
2024-01-08 00:08:20 +0000

    Response header value #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    url = sys.argv[1]
+    reponse = requests.get(url)
+    print(reponse.headers.get("X-Request-Id"))
