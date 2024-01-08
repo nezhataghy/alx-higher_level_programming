2f41ec9c4747f7aef949a7a82aeedb4390b8e5cc
nezhataghy <nezhataghy@gmail.com>
2024-01-08 00:40:05 +0000

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status"""
+
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+
+    if len(sys.argv) == 2:
+        letter = sys.argv[1]
+    else:
+        letter = ""
+    donnee = {"q": letter}
+    reponse = requests.post("http://0.0.0.0:5000/search_user", donnee)
+    try:
+        user = reponse.json()
+        if not (len(user) < 1):
+            print("[{}] {}".format(user["id"], user["name"]))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
