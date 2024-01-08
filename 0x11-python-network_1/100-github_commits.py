d44235f5266c635e2ca1581a06397e0b156ec397
nezhataghy <nezhataghy@gmail.com>
2024-01-08 00:50:51 +0000

    Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    repo = sys.argv[1]
+    user = sys.argv[2]
+
+    url = "https://api.github.com/repos/{}/{}/commits".format(user, repo)
+    response = requests.get(url)
+    donnees = response.json()
+    counter = 0
+    for commit in donnees:
+        if counter > 9:
+            break
+        user = commit.get("commit").get("author").get("name")
+        print("{}: {}".format(commit.get("sha"), user))
+
+        counter += 1
