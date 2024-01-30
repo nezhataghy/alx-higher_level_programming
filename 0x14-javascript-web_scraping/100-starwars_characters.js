57b432b7004a5e6c3be7988aaef11dd7bc7d4d67
nezhataghy <nezhataghy@gmail.com>
2024-01-30 23:47:51 +0000

    Who was playing in this movie?

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -8,13 +8,13 @@ if (!movieId) process.exit();
 const filmsURL = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
 request.get(filmsURL, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-        const charactersInMovie = JSON.parse(body).characters;
-        for (const char of charactersInMovie) {
-            request.get(char, (error, response, body) => {
-                if (error) console.log(error);
-                else console.log(JSON.parse(body).name);
-            });
-        }
+  if (!error && response.statusCode === 200) {
+    const charactersInMovie = JSON.parse(body).characters;
+    for (const char of charactersInMovie) {
+      request.get(char, (error, response, body) => {
+        if (error) console.log(error);
+        else console.log(JSON.parse(body).name);
+      });
     }
+  }
 });
