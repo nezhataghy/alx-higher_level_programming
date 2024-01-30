b202744bf52721c82e501123316226b4de19fa1d
nezhataghy <nezhataghy@gmail.com>
2024-01-30 21:39:28 +0000

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const request = require('request');
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+
+request(url, function (err, data, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + data.statusCode);
+  }
+});
