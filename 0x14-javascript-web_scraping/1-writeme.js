5fa4ae6a6ed0039532efdccb966f2e72a2101f46
nezhataghy <nezhataghy@gmail.com>
2024-01-30 21:31:03 +0000

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+let fs = require('fs');
+
+/* filePath : process.argv[2]; */
+/* content : process.argv[3]; */
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
+    if (err) {
+        console.log(err);
+    }
+});
