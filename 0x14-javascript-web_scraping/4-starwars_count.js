cbf8887d2d582d54bb25b45f455e244e0e1640af
nezhataghy <nezhataghy@gmail.com>
2024-01-30 23:28:24 +0000

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,12 @@
 #!/usr/bin/node
-let request = require('request');
-let url = process.argv[2];
-
-request(url, function (err, data, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    let counter = 0;
-    let films = JSON.parse(body).results;
-    for (let result = 0; result < films.length; result++) {
-      let characters = films[result]['characters'];
-      for (let j = 0; j < characters.length; j++) {
-        if (characters[j] === 'https://swapi.co/api/people/18/' || characters[j] === 'http://swapi.co/api/people/18/') {
-          counter += 1;
-        }
-      }
-    }
-    console.log(counter);
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
