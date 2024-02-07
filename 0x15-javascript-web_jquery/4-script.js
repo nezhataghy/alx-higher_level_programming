8199f057a985f90f323f9798c78489d0ca97d300
nezhataghy <nezhataghy@gmail.com>
2024-02-07 22:23:33 +0000

    Toggle classes

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+$(document).ready(function () {
+    $('DIV#toggle_header').on('click', function () {
+        if ($('header').attr('class') === 'red') {
+            $('header').attr('class', 'green');
+        } else {
+            $('header').attr('class', 'red');
+        }
+    });
+});
