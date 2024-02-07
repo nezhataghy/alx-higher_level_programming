1cea5790ffb8c54f3d68e60080355833fd226c0b
nezhataghy <nezhataghy@gmail.com>
2024-02-07 22:31:40 +0000

    Star wars character

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,1 +1,5 @@
-#!/usr/bin/node
+$(document).ready(function () {
+    $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', (data) => {
+        $('DIV#character').text(data.name);
+    });
+});
