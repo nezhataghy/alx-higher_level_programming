666f93fdd218610002dde53dd8340deeb5e0772e
nezhataghy <nezhataghy@gmail.com>
2024-02-07 22:41:33 +0000

    Say hello to everybody!

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+$(document).ready(function () {
+    $('DIV#add_item').on('click', function () {
+        $('UL.my_list').append('<li>Item</li>');
+    });
+    $('DIV#remove_item').on('click', function () {
+        $('UL.my_list').children().last().remove();
+    });
+    $('DIV#clear_list').on('click', function () {
+        $('li').remove();
+    });
+});
